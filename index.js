35434b9f622c35a2a14585c1eb8c6085f95eeb60
StujiK <138513611+StujiK@users.noreply.github.com>
2024-03-18 14:43:24 +0100

    index2

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-/**
- * 1. read csv file
- * 2. parse to arrays
- * 3. console.log
- */
+const express = require('express')
+const app = express()
+const port = 3000
 
-const fs = require('node:fs');
-try {
-  const data = fs.readFileSync('Inventory.csv', 'utf8');
+app.get('/', (req, res) => {
 
-  // split data into lines
-    const lines = data.split('\r\n');
-    let html = "<html>\n<body>\n<table>\n<tr>\n";
-    // each line
-    for(let i = 0; i <lines.length; i++) { 
-        const fields = lines[i].split(';');
-        html += "<tr>\n";
-        fields.forEach(field => {
-          html += `<td>${field}</td>\n`;
-        });
-        html += "</tr>\n";
-    } 
+  // insert your code from previous task here
 
-    html += "</table>\n</body>\n</html>";
-    console.log(html);
-} catch (err) {
-  console.error(err);
-}
-// expected output (example)
-// [ 'id', 'bild' ]
-// [ 123, 'https://...' ]
-// [ 124, 'https://...' ]
-
+  const fs = require('node:fs');
+  try {
+    const data = fs.readFileSync('Inventory.csv', 'utf8');
   
+    // split data into lines
+      const lines = data.split('\r\n');
+      let html = "<html>\n<body>\n<table>\n<tr>\n";
+      // each line
+      for(let i = 0; i <lines.length; i++) { 
+          const fields = lines[i].split(';');
+          html += "<tr>\n";
+          fields.forEach(field => {
+            html += `<td>${field}</td>\n`;
+          });
+          html += "</tr>\n";
+      } 
+  
+      html += "</table>\n</body>\n</html>";
+      console.log(html);
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.send(html)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
